21ade30c44ff4d8862b3acb408eabf0f95471b0a
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2024-04-03 01:35:18 +0000

    navigation with drop down added

--- src/js/main.js
+++ src/js/main.js
@@ -37,31 +37,3 @@ function openMenu () {
 }
 
 
-
-
-// ============== DropDown functionality =============
-document.addEventListener("click", e => {
-    const isDropdownButton = e.target.matches("[data-dropdown-button]");
-    if(!isDropdownButton && e.target.closest("[data-dropdown]") != null)
-    return
-
-    let currentDropdown
-    if(isDropdownButton) {
-        currentDropdown = e.target.closest('[data-dropdown]');
-        currentDropdown.classList.toggle('active');
-        // navBar.classList.toggle('no-shadow');
-    }
-
-    document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
-        if (dropdown === currentDropdown) return
-        dropdown.classList.remove("active")
-    })
-})
-
-
-
-// =============== Remove shadow on Navbar when a link is clicked ===============
-const navBar = document.querySelector("nav-bar");
-navItem.forEach(item =>{
-    // item.addEventListener('click', console.log('you clicked'));
-})
