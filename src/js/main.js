d378a5bc30e1d1c1373543d11f349c671bec6a6e
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2024-04-30 15:22:44 +0000

    navigation hides and appears on scrollY

--- src/js/main.js
+++ src/js/main.js
@@ -59,3 +59,26 @@ document.addEventListener("click", e => {
 
 
 
+
+
+
+// Hide Header on scroll down
+
+const header = document.querySelector("nav");
+// Set the initial scroll position
+let lastScrollPosition = 0;
+// // Add an event listener for the scroll event
+window.addEventListener("scroll", () => {
+  // Get the current scroll position
+  let currentScrollPosition = window.scrollY;
+  
+  if (currentScrollPosition - lastScrollPosition > 0) {
+    // If the scroll direction is down and the user has scrolled past the navbar, hide the navbar
+    header.classList.add("hide");
+  } else {
+     // If the scroll direction is up or the user is at the top of the page, show the navbar
+    header.classList.remove("hide");
+  }
+  // Set the last scroll position to the current scroll position
+  lastScrollPosition = currentScrollPosition;
+})
