cef402a2600f1f98f1ddf650c9628022ad661c60
Sarfo Carbell Kofi <sarfocarbell@gmail.com>
2024-06-18 12:30:25 +0000

    odometer ready for trail

--- src/js/odometer.js
+++ src/js/odometer.js
@@ -1,14 +1,36 @@
-
 const createOdometer = (el, value) => {
-    const odometer = new Odometer ({
-        el: el,
-        value: 0,
+    const odometer = new Odometer({
+      el: el,
+      value: 0,
     });
-
-   Odometer.update(value);
-};
-
-
-
-const customerOdometer = document.querySelector(".customer-odometer");
-createOdometer(customerOdometer, 1200);
+  
+    let hasRun = false;
+  
+    const options = {
+      threshold: [0, 0.9],
+    };
+  
+    const callback = (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          if (!hasRun) {
+            odometer.update(value);
+            hasRun = true;
+          }
+        }
+      });
+    };
+  
+    const observer = new IntersectionObserver(callback, options);
+    observer.observe(el);
+  };
+  
+  const subscribersOdometer = document.querySelector(".customer-odometer");
+  createOdometer(subscribersOdometer, 1200);
+  
+  const videosOdometer = document.querySelector(".client-odometer");
+  createOdometer(videosOdometer, 5000);
+  
+  const projectsOdometer = document.querySelector(".claim-odometer");
+  createOdometer(projectsOdometer, 7500);
+  
